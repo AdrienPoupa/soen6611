566b26445db8261b24029ee560f27b382ca235a9
Adrien Poupa <adrien@poupa.fr>
2018-05-17 16:12:52 -0400

    Update min.r

--- min.r
+++ min.r
@@ -1,14 +1,12 @@
 a <- c(0:25)
 
 min_custom <- function(array) {
-  value <- array[0]
+  value = array[[1]]
   
   for(i in array) {
-    #if (i < value) {
-    #  value <- i
-    #}
-    print(i)
-    print(value)
+    if (i < value) {
+      value = i
+    }
   }
 
   return (value)
