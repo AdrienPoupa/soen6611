b02bc4d507857366b0bb1d1da2b458578c498202
Ksenia Popova <p.ksyusha@gmail.com>
2018-06-01 10:57:50 -0400

    Updated mode function. It uses the Radix sort to sort the array and then works with a sorted array to find a mode. Mode is not unique, there can be multiple modes in an array.

--- mode.r
+++ mode.r
@@ -1,28 +1,42 @@
-#mode function
-#returns most frequent number in array
+#R version 3.3.2 
 #Author: Ksenia Popova
+#Returns most frequent number in array
+#May 2018
 
-mode <- function(array) {
-  max <- 0
-  value <- array[1]
-  currentAmount <- 0
+mode <- function(array, n) {
+
+  max <- 1
+  currentAmount <- 1
+  n <- n-1
+  modeRes <- c()
   
-  for (i in array) {
-    currentAmount <- 0
-    for (j in array){
-      if (j == i) {
-        currentAmount <- currentAmount+1
-        
-        if (currentAmount>max){
-          value <- i
-          max <- currentAmount
-        }
+  for (i in c(1:n)) {
+    if (array[i]==array[i+1]){
+      currentAmount <- currentAmount+1
+    }
+    else {
+      if(currentAmount == max) {
+        modeRes <- c(modeRes, array[i])
       }
+      if (currentAmount > max) {
+        max <- currentAmount
+        modeRes <- array[i]
+      }
+      currentAmount <- 1
     }
   }
-  return (value)
+  
+  if (currentAmount == max)
+    modeRes <- c(modeRes, array[n+1])
+  if (currentAmount > max) 
+    modeRes <- array[i]
+  
+  return (modeRes)
 }
 
-array <- c(1,2,2,1,2,1,3,1,4,4)
-mode(array)
+
+array <- c(1,2,2,2,2,3,3,3,4,4,4,4)
+n <- sizeoflist(array)
+sortedArray <- radixsort(array, n)
+mode(sortedArray, n)
 
