a82d8cb28e56bd9ababd13f8298d04c68de46e09
fned <fned@users.noreply.github.com>
2018-06-04 18:12:05 -0400

    Unit Testing Added.
    also added the pdf for testing documentation.

---
+++ UnitTesting.r
@@ -0,0 +1,43 @@
+#R version 3.3.2 
+#Author: Farhan Shaheen
+#Unit Testing in R
+#June 2018
+
+source("DescriptiveStatistics.r")
+source("data.r")
+
+list <- read.csv("data.csv",header=F)$V1
+
+# https://www.calculatorsoup.com/calculators/statistics/mean-median-mode.php
+are_equal(max_custom(list), 100)
+are_equal(min_custom(list), 1)
+are_equal(mean_custom(list), 47.324)
+are_equal(median(list), 46)
+are_equal(mode(list), c(22))
+
+# http://www.calculator.net/standard-deviation-calculator.html
+are_equal(std(list), 28.733475888528)
+
+# R compare with R built-in functions
+max(list)
+min(list)
+mean(list)
+median(list)
+
+# data <- c(93, 45, 75, 96, 80, 45, 2, 66, 75)
+# Test function only
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+getmode(list)
+sd(list)
+
+are_equal(max_custom(list), max(list))
+are_equal(min_custom(list), min(list))
+are_equal(mean_custom(list), mean(list))
+are_equal(median(list), median(list))
+are_equal(mode(list), getmode(list))
+are_equal(std(list), sd(list))
+
