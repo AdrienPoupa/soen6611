5c0e17bdd7012bd76ae64e9257b804b36b266e2b
AdrienPoupa <adrien@poupa.fr>
2018-06-09 20:27:26 -0400

    Convert driver into a class

--- driver.r
+++ driver.r
@@ -7,23 +7,33 @@
 source("descriptive_statistics.r")
 source("data_provider.r")
 
-dp <- data_provider()
-random_grades <- dp$get_random_numbers()
+driver <- setRefClass("driver",
+     fields = list(dp = "data_provider", ds = "descriptive_statistics"),
+     methods = list(
+       constructor = function(){
+         dp <<- data_provider()
+         random_grades <- dp$get_random_numbers()
+         
+         ds <<- descriptive_statistics(grades = random_grades)
+         
+         max_grade <- ds$max_grade()
+         min_grade <- ds$min_grade()
+         grades_mean <- ds$grades_mean()
+         grades_median <- ds$grades_median()
+         grades_mode <- ds$grades_mode()
+         
+         #standard deviation
+         std_dev <- ds$grades_std_dev()
+         
+         print(paste0("maximum grade: ", max_grade))
+         print(paste0("minimum grade: ", min_grade))
+         print(paste0("grades mean: ", grades_mean))
+         print(paste0("grades median: ", grades_median))
+         print(paste0("grades mode: ", grades_mode))
+         print(paste0("grades standard deviation: ", std_dev))
+       }
+     )
+)
 
-ds <- descriptive_statistics(grades = random_grades)
-
-max_grade <- ds$max_grade()
-min_grade <- ds$min_grade()
-grades_mean <- ds$grades_mean()
-grades_median <- ds$grades_median()
-grades_mode <- ds$grades_mode()
-
-#standard deviation
-std_dev <- ds$grades_std_dev()
-
-print(paste0("maximum grade: ", max_grade))
-print(paste0("minimum grade: ", min_grade))
-print(paste0("grades mean: ", grades_mean))
-print(paste0("grades median: ", grades_median))
-print(paste0("grades mode: ", grades_mode))
-print(paste0("grades standard deviation: ", std_dev))
+main<-driver()
+main$constructor()
