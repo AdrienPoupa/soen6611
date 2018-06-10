81640a2d7dab9252f23ec59c78aabd041aa5dc0a
Jehlum <k2@karakrum>
2018-06-10 16:03:26 -0400

    reformating

--- unit_testing.r
+++ unit_testing.r
@@ -10,56 +10,63 @@ source("descriptive_statistics.r")
 source("data_provider.r")
 
 unit_testing <- setRefClass("unit_testing",
-        fields = list(dp = "data_provider", ds = "descriptive_statistics"),
-        methods = list(
-          constructor = function(){
-            dp <<- data_provider()
-            random_grades <- dp$get_random_numbers()
-            
-            ds <<- descriptive_statistics(grades = random_grades)
-            
-            max_grade <- ds$max_grade()
-            min_grade <- ds$min_grade()
-            grades_mean <- ds$grades_mean()
-            grades_median <- ds$grades_median()
-            grades_mode <- ds$grades_mode()
-            
-            #standard deviation
-            std_dev <- ds$grades_std_dev()
-            
-            # https://www.calculatorsoup.com/calculators/statistics/mean-median-mode.php
-            print(paste0(are_equal(max_grade, 100)))
-            print(paste0(are_equal(min_grade, 1)))
-            print(paste0(are_equal(grades_mean, 47.324)))
-            print(paste0(are_equal(grades_median, 46)))
-            print(paste0(are_equal(grades_mode, c(22))))
-            
-            # http://www.calculator.net/standard-deviation-calculator.html
-            are_equal(std_dev, 28.719105557103)
-            
-            # R compare with R built-in functions
-            max(random_grades$items)
-            min(random_grades$items)
-            mean(random_grades$items)
-            median(random_grades$items)
-            
-            # data <- c(93, 45, 75, 96, 80, 45, 2, 66, 75)
-            # Test function only
-            get_mode <- function(v) {
-              uniqv <- unique(v)
-              uniqv[which.max(tabulate(match(v, uniqv)))]
-            }
-            
-            get_mode(random_grades$items)
-            sd(random_grades$items)
-            
-            print(paste0(are_equal(max_grade, max(random_grades$items))))
-            print(paste0(are_equal(min_grade, min(random_grades$items))))
-            print(paste0(are_equal(grades_mean, mean(random_grades$items))))
-            print(paste0(are_equal(grades_median, median(random_grades$items))))
-            print(paste0(are_equal(grades_mode, get_mode(random_grades$items))))
-          }
-        )
+  fields = list(dp = "data_provider", ds = "descriptive_statistics"),
+  methods = list(
+    constructor = function(){
+      dp <<- data_provider()
+      random_grades <- dp$get_random_numbers()
+      
+      ds <<- descriptive_statistics(grades = random_grades)
+      
+      max_grade <- ds$max_grade()
+      min_grade <- ds$min_grade()
+      grades_mean <- ds$grades_mean()
+      grades_median <- ds$grades_median()
+      grades_mode <- ds$grades_mode()
+      
+      #standard deviation
+      std_dev <- ds$grades_std_dev()
+      
+      cat("Comparing with manually calculated values\n")
+      cat("--------------------------------------\n")
+      
+      # https://www.calculatorsoup.com/calculators/statistics/mean-median-mode.php
+      print(paste0("max grade test: ", are_equal(max_grade, 100)))
+      print(paste0("min grade test: ", are_equal(min_grade, 1)))
+      print(paste0("grades mean test: ", are_equal(grades_mean, 47.324)))
+      print(paste0("grades median test: ", are_equal(grades_median, 46)))
+      print(paste0("grades mode test: ", are_equal(grades_mode, c(22))))
+      
+      # http://www.calculator.net/standard-deviation-calculator.html
+      print(paste0("Standard Deviation test: ",are_equal(std_dev, 28.719105557103)))
+      
+      # R compare with R built-in functions
+      
+      max(random_grades$items)
+      min(random_grades$items)
+      mean(random_grades$items)
+      median(random_grades$items)
+      
+      # data <- c(93, 45, 75, 96, 80, 45, 2, 66, 75)
+      # Test function only
+      get_mode <- function(v) {
+        uniqv <- unique(v)
+        uniqv[which.max(tabulate(match(v, uniqv)))]
+      }
+      
+      get_mode(random_grades$items)
+      sd(random_grades$items)
+      
+      cat("\nComparing with Built-in R functions\n")
+      cat("--------------------------------------\n")
+      print(paste0("are_equal(max_grade, max): ", are_equal(max_grade, max(random_grades$items))))
+      print(paste0("are_equal(min_grade, min): ", are_equal(min_grade, min(random_grades$items))))
+      print(paste0("are_equal(grades_mean, mean): ", are_equal(grades_mean, mean(random_grades$items))))
+      print(paste0("are_equal(grades_median, median): ", are_equal(grades_median, median(random_grades$items))))
+      print(paste0("are_equal(grades_mode, get_mode): ", are_equal(grades_mode, get_mode(random_grades$items))))
+      print(paste0("are_equal(std_dev, sd): ", are_equal(std_dev, sd(random_grades$items))))
+    }
+  )
 )
 
 main<-unit_testing()
